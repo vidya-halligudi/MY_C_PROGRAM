919434451f057dd3d4e8d0bf1bc81a6f8f951aef
vidya-halligudi <vidyahalligudi@gmail.com>
2025-11-18 11:26:56 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i ,sum=0,n=4;
+    printf("enter the value of n \n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {sum=sum+i;
+    }
+    printf("sum of first %d natural number is %d\n",n,sum);
+}
+    
+    
+    
