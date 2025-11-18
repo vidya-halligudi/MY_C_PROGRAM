3b3b91a8dffc8ecacae5e0bc2c147045f19d978d
vidya-halligudi <vidyahalligudi@gmail.com>
2025-11-18 11:16:29 +0530

    Create if else ladder.c

---
+++ if else ladder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    printf("enter your marks");
+    
+     if (marks >=90)
+  {  printf("excellent");}
+    
+    else if( marks>=80)
+   { printf("good");}
+    else if (marks>=50)
+    
+  {  printf("average");}
+    else {printf("just pass");}
+
+    return 0;
+}
