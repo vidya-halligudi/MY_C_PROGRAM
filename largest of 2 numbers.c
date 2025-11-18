f8eae4c76d700227b587aba63acc7db155980423
vidya-halligudi <vidyahalligudi@gmail.com>
2025-11-18 13:54:26 +0530

    Create largest of 2 numbers.c

---
+++ largest of 2 numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the value of a and b");
+    scanf("%d%d",&a,&b);
+    if (a>0)
+    printf("a is large");
+    else ("b is large");
+
+    return 0;
+}
