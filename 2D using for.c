b5d15256d567e01181ca6faca39242d6fbd76df6
vidya-halligudi <vidyahalligudi@gmail.com>
2025-11-19 11:13:40 +0530

    Create 2D using for.c

---
+++ 2D using for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int pen[2][3]={
+        {1,2,3},
+        {4,5,6}
+    };
+    for (int i=0;i<=2;i++){
+        for(int j=0;j<=3;j++){
+            printf("%d\n" ,pen[i][j]);
+        }
+    }
+
+    return 0;
+}
