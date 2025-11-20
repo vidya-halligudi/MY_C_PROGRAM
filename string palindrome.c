bc4c11c4ba3198478f174a20215e7ba849066233
vidya-halligudi <vidyahalligudi@gmail.com>
2025-11-20 11:49:01 +0530

    Create string palindrome.c

---
+++ string palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50],rev[50];
+    int i,len;
+    printf("enter a string");
+    scanf("%s",str);
+    len=strlen(str);
+    for(i=0;i<len;i++){
+        rev[i]=str[len-1-i];
+        rev[len]='\0';
+        if (strcmp(str,rev)==0)
+        printf("it is palindrome");
+        else 
+        printf("not palindrome");
+    }
